9a1068127422b50ec293659393aca1a869925074
Ali Hassan Abdulhadi <a.h.alsaady1998@gmail.com>
2025-08-09 17:46:41 +0300

    init

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var (
+	aesKey, _ = base64.StdEncoding.DecodeString("szqC6Qw5k7o2ztNcJsiDBbT+bKqHRF8kUCSVbjw3QKA=")
+)
+
+func pkcs5Padding(data []byte, blockSize int) []byte {
+	padLen := blockSize - len(data)%blockSize
+	padding := bytes.Repeat([]byte{byte(padLen)}, padLen)
+	return append(data, padding...)
+}
+
+func encryptAES(plaintext []byte, key []byte) (string, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	iv := make([]byte, block.BlockSize())
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	padded := pkcs5Padding(plaintext, block.BlockSize())
+
+	ciphertext := make([]byte, len(padded))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext, padded)
+
+	finalData := append(iv, ciphertext...)
+	return base64.StdEncoding.EncodeToString(finalData), nil
+}
+
+func main() {
+	data := map[string]interface{}{
+		"id":               "UE1A." + time.Now().Format("060102.150405"), // dynamic id with date-time
+		"type":             "user",
+		"brand":            "ali",
+		"model":            "sdk_gphone64_x86_64",
+		"nonce":            uuid.New().String(), // new UUID each run
+		"device":           "emu64xa",
+		"display":          "UE1A." + time.Now().Format("060102.150405"),
+		"product":          "sdk_gphone64_x86_64",
+		"platform":         "android",
+		"timestamp":        fmt.Sprintf("%d", time.Now().UnixMilli()), // current timestamp in ms
+		"manufacturer":     "Google",
+		"serialNumber":     "unknown",
+		"version.baseOS":   "",
+		"isPhysicalDevice": true,
+		"version.codename": "REL",
+	}
+
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		panic(err)
+	}
+
+	encrypted, err := encryptAES(jsonData, aesKey)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Encrypted Base64:")
+	fmt.Println(encrypted)
+}
